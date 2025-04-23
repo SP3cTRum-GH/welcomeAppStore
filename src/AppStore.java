85d02313ffe07ec07c84ba6590357547d0875b85
SP3cTRum-GH <min1324354@gmail.com>
2025-04-24 00:16:06 +0900

    step1

---
+++ src/AppStore.java
@@ -0,0 +1,12 @@
+public class AppStore {
+    public static void main(String[] args) {
+        System.out.println("***************************************************");
+        System.out.println("\t\t\t\t" + "Welcome AppStore");
+        System.out.println("***************************************************");
+        System.out.println(" 1. 등록된 앱 정보 확인하기 \t4.앱 업데이트");
+        System.out.println(" 2. 다운로드된 앱 목록 보기\t5. 앱 삭제하기");
+        System.out.println(" 3. 앱검색 및 설치 \t\t6. 결제내역 표시하기");
+        System.out.println(" 8. 종료");
+        System.out.println("***************************************************");
+    }
+}
